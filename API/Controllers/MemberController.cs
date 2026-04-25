78ec84d71bc6f614af6d60c645aabd68c85e00cb
Nicholas Trimble <nicktrimble07@gmail.com>
2026-04-25 16:17:14 -0500

    Initial push of project

---
+++ API/Controllers/MemberController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")] // localhost:5001/api/members auto-maps to the controller name
+    [ApiController]
+    public class MembersController (AppDbContext context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+        {
+            var members = await context.Users.ToListAsync();
+            return members;
+        }
+
+        [HttpGet("{id}")] // localhost:5001/api/members/bob-id
+        public async Task<ActionResult<AppUser>> GetMember(String Id)
+        {
+            var member = await context.Users.FindAsync(Id);
+            if (member == null) return NotFound();
+            return member;
+        }
+    }
+}
